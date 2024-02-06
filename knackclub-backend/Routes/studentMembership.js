c04b03130fcc827c908bf4eaa12d9797fa99b80a
Krishna <kdwivedi343@gmail.com>
2024-02-06 10:09:52 +0530

    Resolved database conflicts

--- knackclub-backend/Routes/studentMembership.js
+++ knackclub-backend/Routes/studentMembership.js
@@ -9,7 +9,7 @@ route.post('/userdetails', [
 ],async(req,res)=>{
     try {
 
-        const result = validationResult(req);
+        const result = await validationResult(req);
 
         if (!result.isEmpty()) {
             return res.status(400).json({ errors: result.array() });
