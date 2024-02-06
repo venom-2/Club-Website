b1be8ea6670cc15d414965f6272760b4485dbde0
Krishna <kdwivedi343@gmail.com>
2024-02-06 13:57:33 +0530

    test-06

--- knackclub-backend/index.js
+++ knackclub-backend/index.js
@@ -1,10 +1,15 @@
 const connectToDatabase = require('./db');
 const express = require('express');
+const cors = require('cors')
 
 connectToDatabase();
 
 const app = express();
 const port = 3001;
+
+app.use(cors());
+
+app.use(express.json());
 
 // app.use(express.json()); // Add this line to parse JSON requests
 // app.use(express.urlencoded({ extended: true })); // Add this line to parse URL-encoded requests
