a534fc68392e6dfe5a23e0e00e5db821a4136c42
Krishna <kdwivedi343@gmail.com>
2024-02-06 13:09:45 +0530

    test-04

--- knackclub-backend/index.js
+++ knackclub-backend/index.js
@@ -6,8 +6,8 @@ connectToDatabase();
 const app = express();
 const port = 3001;
 
-app.use(express.json()); // Add this line to parse JSON requests
-app.use(express.urlencoded({ extended: true })); // Add this line to parse URL-encoded requests
+// app.use(express.json()); // Add this line to parse JSON requests
+// app.use(express.urlencoded({ extended: true })); // Add this line to parse URL-encoded requests
 
 app.get('/',(req,res)=>{
     console.log("Hello Knack");
